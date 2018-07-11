fbe4d97d4e3fa723f4c520dfa02c57b69f0fb879
ibnubs <ibnubs@gmail.com>
2018-07-11 14:01:47 +0700

    add ex3

---
+++ week-3/rabu/notekelas.js
@@ -0,0 +1,58 @@
+//catatan Array
+
+//tambahkan tulisan Arr di tulisan variabelnya....untuk menandakan bahwa data tersebut array
+
+//type data primitiv dan stucture
+//bahwa dalam aray bisa ditulis dari ke 3 data
+// array itu index atau alamat
+
+//var animals = []
+
+
+//pelajaran hari ini adalah multidimensional array
+
+//var countries = [ 'canada', 'germany', 'japan']
+
+// //contoh 1,,------------------------------------------
+// var asiaCountries = ['INA', 'Japan', 'korea']
+// var eropeCountries = ['GER', 'ENG', 'FRANCE']
+// var americaCountries = ['usa', 'can', 'arg']
+
+// var countries = [[asiaCountries],[eropeCountries],[americaCountries]]
+
+
+// console.log(countries)
+// //end contoh 1---------------------------------------
+
+
+
+//contoh 2,,------------------------------------------
+// var asiaCountries = ['INA', 'Japan', 'korea']
+// var eropeCountries = ['GER', 'ENG', 'FRANCE']
+// var americaCountries = ['usa', 'can', 'arg']
+
+// var countries = []
+// countries.push (asiaCountries)
+
+
+// console.log(countries)
+//end contoh 2---------------------------------------
+
+
+//contoh 3,,------------------------------------------
+var asiaCountries = ['INA', 'Japan', 'korea']
+var eropeCountries = ['GER', 'ENG', 'FRANCE']
+var americaCountries = ['usa', 'can', 'arg']
+
+var countries = []
+countries.push (asiaCountries, eropeCountries, americaCountries)
+
+
+for (var i=0; i)
+//end contoh 3---------------------------------------
+
+
+//contoh4
+var countries = [['asia'],['uk'],['us']]
+console.log(countries)
+//end contoh4
