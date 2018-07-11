518b0358a60f9c682651d37f7a42b796ded31429
ibnubs <ibnubs@gmail.com>
2018-07-11 11:34:23 +0700

    upload ex2-w3-p0

--- week-3/selasa/exercise-2.js
+++ week-3/selasa/exercise-2.js
@@ -1,7 +1,8 @@
-exercise-2.Mengakses Nilai Dalam Array
-
-
-function balikString = 'hello world'
-
-balikString.length
-
+//exercise-2.Mengakses Nilai Dalam Array
+function balikKata (kata){
+    var tampungKata = '';
+    for (var i= kata.length-1; i>=0; i--){
+        tampungKata += kata[i];
+    } return tampungKata;
+}
+console.log(balikKata("Hello World"))
