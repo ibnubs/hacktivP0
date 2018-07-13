79c2cbea5577311d032f4d3512df8e157ad13e6c
ibnubs <ibnubs@gmail.com>
2018-07-13 23:56:33 +0700

    addex15

---
+++ week-3/jumat/exercise-15.js
@@ -0,0 +1,47 @@
+//exercise-15--> mengelompokan hewan
+
+
+/*
+problem
+Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
+
+Contoh jika animals adalah ["ayam", "kucing", "bebek", "bangau", "zebra"]
+
+maka akan menampilkan output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ], [ "zebra" ] ]
+
+Urutan hewan dalam array setiap pengelompokan huruf tidak penting.
+*/
+
+
+
+  function groupAnimals(animals) {
+    animals.sort(function(a,b){ return a > b})
+    var cek = []
+    var kategori = []
+    
+    for(var aa = 0; aa < animals.length; aa++){
+      var index = -1
+    
+      for (let bb = 0; bb < kategori.length; bb++) {
+        if (animals[aa][0] === kategori[bb]) {
+          index = bb
+        }
+      }
+    
+      
+      if(index === -1){
+         kategori.push(animals[aa][0])
+         cek.push([animals[aa]])
+      } else {
+        cek[index].push(animals[aa])
+      }
+    
+    }
+    return cek;
+    }
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
